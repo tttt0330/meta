e5d8e49605109c476c660172427a9c562d984ae5
tttt0330 <tiffany20080330@gmail.com>
2024-05-27 15:07:55 +0800

    food

---
+++ ssshfood/data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "馬路轉角",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7010391316584652,
+        "pitch": 0.369056833040748,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7603652139768009,
+          "pitch": 0.08207242420241023,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7459381701712493,
+          "pitch": 0.1703047840168832,
+          "title": "馬路轉角",
+          "text": "每天早上交通十分繁忙的地帶，每次都可以看到大量高中生聚集在此，因此每天紅綠燈都要等超久"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "早餐店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.9884138435450858,
+        "pitch": 0.21780631260091354,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4834685496458366,
+          "pitch": 0.21899681082056688,
+          "rotation": 7.853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.188903542735357,
+          "pitch": 0.33674179467239185,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.739567195388151,
+          "pitch": 0.013791755858738597,
+          "title": "早餐店",
+          "text": "這邊有好幾間咖啡廳、早餐店，是還沒吃早餐的學生的好選擇!"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "校園轉角處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4819855766179026,
+        "pitch": 0.2588023302082689,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5132327656720523,
+          "pitch": 0.3478429449681837,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.6214615838998476,
+          "pitch": 0.26224015597609807,
+          "rotation": 0,
+          "target": "3-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5680234813012008,
+          "pitch": 0.023471096624589904,
+          "title": "校園轉角",
+          "text": "沿著這條路一直走就到非常受大家歡迎的後巷!不但物價合理，而且離學校也超級近，更重要的是那邊的食物都很美味~"
+        }
+      ]
+    },
+    {
+      "id": "3-1",
+      "name": "後巷餐廳1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9339895919644121,
+        "pitch": 0.4175602440395636,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9339895919644121,
+          "pitch": 0.4175602440395636,
+          "rotation": 0.7853981633974483,
+          "target": "4-2"
+        },
+        {
+          "yaw": -0.12815479735326285,
+          "pitch": 0.38706624191227057,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4085744841513215,
+          "pitch": 0.22954250322655412,
+          "title": "後巷",
+          "text": "從這裡開始正式進入後巷，每天放學都會有很多晚上還要去補習班的學生在這裡吃晚餐"
+        }
+      ]
+    },
+    {
+      "id": "4-2",
+      "name": "後巷餐廳2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.908695326472116,
+        "pitch": 0.21301284709787005,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9862610291938747,
+          "pitch": 0.35249680494582414,
+          "rotation": 0.7853981633974483,
+          "target": "5-3"
+        },
+        {
+          "yaw": 2.650367218718884,
+          "pitch": 0.7480124816708145,
+          "rotation": 3.9269908169872414,
+          "target": "3-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9428944121721132,
+          "pitch": 0.17881376139787442,
+          "title": "後巷餐廳",
+          "text": "後巷真的賣超級多東西，從一般餐廳、便利商店、美味的小吃......保證在這裡可以吃得飽飽的!"
+        }
+      ]
+    },
+    {
+      "id": "5-3",
+      "name": "後巷餐廳3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.584806398757051,
+        "pitch": 0.31913196248007836,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4798463048279027,
+          "pitch": 0.732443812323428,
+          "rotation": 3.9269908169872414,
+          "target": "4-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.491500401280854,
+          "pitch": 0.2529310110068437,
+          "title": "後巷餐廳",
+          "text": "這裡賣的是水果和飲料，去補習班前買一杯冰冰的飲料超讚!"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
