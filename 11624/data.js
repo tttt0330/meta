d5d967a64c24f5f08ee2f702b46a7a656b347f75
tttt0330 <tiffany20080330@gmail.com>
2024-05-27 15:05:27 +0800

    11624

---
+++ 11624/data.js
@@ -0,0 +1,254 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sukiya",
+      "name": "sukiya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03124608731274847,
+          "pitch": 0.07273991736538399,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.979511426979255,
+          "pitch": 0.01720039272150231,
+          "title": "sykiya",
+          "text": "他們家的牛井很好吃"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "八方 便當",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.100214084589478,
+          "pitch": 0.02245574655077398,
+          "rotation": 0,
+          "target": "0-sukiya"
+        },
+        {
+          "yaw": 0.7543093088758024,
+          "pitch": 0.06990327999179513,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6893946101499981,
+          "pitch": 0.09156427539631551,
+          "title": "八方雲集",
+          "text": "鍋貼以及乾麵都不錯"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "星巴克",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.139208147498259,
+          "pitch": 0.16614347878079627,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4034695739650775,
+          "pitch": -0.18990177541935083,
+          "title": "星巴克",
+          "text": "一間美國的連鎖咖啡廳"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "麻古",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5645605845736661,
+          "pitch": 0.10116757560129841,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -2.991430273253272,
+          "pitch": 0.23010409542559884,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9790483023369454,
+          "pitch": 0.1934303499762784,
+          "title": "麻古",
+          "text": "我個人大推鐵觀音奶茶"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "溫州大餛飩",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.026504779121186317,
+          "pitch": 0.014722087728927136,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.041572096387963,
+          "pitch": -0.11463965211838989,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.812332418487622,
+          "pitch": -0.03518734761383158,
+          "title": "溫州大餛飩",
+          "text": "他們的招牌也就是餛飩非常好吃"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
